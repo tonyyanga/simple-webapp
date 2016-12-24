8f9cf32915b3c76e0543d840205af03616526656
Tony Yang <tony@ycyang.me>
2016-12-24 20:53:50 +0800

    fix AJAX IE6 compatibility

--- www/ajax.js
+++ www/ajax.js
@@ -1,6 +1,12 @@
 function ajaxFetch(resourcesTag, resourcesLink) {
 	// Fetch information at resourcesLink and update it to resourcesTag
-	var xhttp = new XMLHttpRequest();
+	var xhttp
+	if (window.XMLHttpRequest) {
+		xhttp = new XMLHttpRequest();
+	} else {
+		// for IE6 or earlier compatibility
+		xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			document.getElementById(resourcesTag).innerHTML = this.responseText;
